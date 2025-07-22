615900b1b6dfbd53ddbd8d4e4a5dda5c70403a2a
Pascalpex <pascalpex@gmail.com>
2025-07-22 09:19:46 +0200

    More rebranding

---
+++ nycticorax-server/src/main/java/org/dreeam/nycticorax/version/NycticoraxVersionFetcher.java
@@ -0,0 +1,16 @@
+package org.dreeam.nycticorax.version;
+
+import org.galemc.gale.version.AbstractPaperVersionFetcher;
+
+public class NycticoraxVersionFetcher extends AbstractPaperVersionFetcher {
+
+    public NycticoraxVersionFetcher() {
+        super(
+            "https://github.com/Winds-Studio/Nycticorax",
+            "Winds Studio",
+            "Nycticorax",
+            "Winds-Studio",
+            "Nycticorax"
+        );
+    }
+}
